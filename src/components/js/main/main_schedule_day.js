b36ea869bf2b7765df93734bfd54da52cb57c15b
Park Jun Hyeong <jh981109@naver.com>
2023-07-14 05:04:45 +0900

    schedule

---
+++ src/components/js/main/main_schedule_day.js
@@ -0,0 +1,25 @@
+import day from "../../css/main/main_schedule_day.module.css";
+
+function Day() {
+  return (
+    <div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box}></div>
+      <div className={day.day_box_last}></div>
+    </div>
+  );
+}
+
+export default Day;
