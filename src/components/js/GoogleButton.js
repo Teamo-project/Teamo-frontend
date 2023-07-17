11e39bd3c4c0f69f8034dbfc5e6d929c154e4c71
Cwonseo <dnjstj88@naver.com>
2023-07-17 22:16:34 +0900

    로그인 시 clientId 넘기기, Main페이지로 이동하기 구현

--- src/components/js/GoogleButton.js
+++ src/components/js/GoogleButton.js
@@ -1,22 +1,33 @@
-import { useGoogleLogin } from "@react-oauth/google";
-import styles from "../css/GoogleButton.module.css";
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import jwtDecode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const GoogleButton = () => {
-  const onClick = useGoogleLogin({
-    onSuccess: (credentialResponse) => {
-      console.log(credentialResponse);
-    },
-    onError: () => {
-      console.log("Login Failed");
-    },
-    flow: "auth code",
-  });
+  const navigate = useNavigate();
 
   return (
-    <button className={styles.Google} onClick={onClick}>
-      <i class="fa-brands fa-google"></i>
-      <span> Google 계정으로 로그인</span>
-    </button>
+    <GoogleLogin
+      buttonText="Login"
+      onSuccess={(res) => {
+        const inFo = jwtDecode(res.credential);
+
+        fetch("http://localhost:3000/login", {
+          method: "POST",
+          body: JSON.stringify({
+            clientId:
+              "617889757552-kkjsgl2e9rs1heefavl8macrgnsvh891.apps.googleusercontent.com",
+          }),
+        })
+          .then((res) => {
+            res.json();
+            navigate("/Main");
+          })
+          .then(console.log(res));
+      }}
+      onError={() => {
+        console.log("FAIL");
+      }}
+    />
   );
 };
 export default GoogleButton;
